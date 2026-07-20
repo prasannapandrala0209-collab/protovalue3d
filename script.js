462ff9a9858010fc4b0146eb2653e13c68124824
prasannapandrala0209-collab <prasannapandrala0209@gmail.com>
2026-07-20 15:55:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -140,3 +140,16 @@ link.classList.add("active");
 });
 
 });
+// Scroll Progress Bar
+
+window.addEventListener("scroll",()=>{
+
+let winScroll=document.body.scrollTop||document.documentElement.scrollTop;
+
+let height=document.documentElement.scrollHeight-document.documentElement.clientHeight;
+
+let scrolled=(winScroll/height)*100;
+
+document.getElementById("progress-bar").style.width=scrolled+"%";
+
+});
