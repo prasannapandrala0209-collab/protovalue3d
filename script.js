fc67f76f32407ef13bda5f3179d34de5444dc5a5
prasannapandrala0209-collab <prasannapandrala0209@gmail.com>
2026-07-20 15:29:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,15 @@ document.querySelectorAll('nav a').forEach(link => {
     }
   });
 });
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll("section").forEach(section=>{
+    section.classList.add("fade");
+    observer.observe(section);
+});
