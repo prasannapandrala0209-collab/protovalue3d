5b64383c00173a7130dfe1cad25dcd612d2487fa
prasannapandrala0209-collab <prasannapandrala0209@gmail.com>
2026-07-20 15:40:38 +0530

    Added Animated Statistics Counter

--- script.js
+++ script.js
@@ -50,3 +50,38 @@ document.querySelectorAll("section").forEach(section=>{
     section.classList.add("fade");
     observer.observe(section);
 });
+// Animated Counter
+
+const counters = document.querySelectorAll(".counter");
+
+const speed = 200;
+
+counters.forEach(counter=>{
+
+const updateCount=()=>{
+
+const target=+counter.getAttribute("data-target");
+
+const count=+counter.innerText;
+
+const inc=target/speed;
+
+if(count<target){
+
+counter.innerText=Math.ceil(count+inc);
+
+setTimeout(updateCount,10);
+
+}
+
+else{
+
+counter.innerText=target;
+
+}
+
+}
+
+updateCount();
+
+});
